0cee5a46b98aaefff80e2edb937fc51210bfee93
Si Thu Min Aung <sithuminaung2468@gmail.com>
2025-11-09 22:24:37 +0630

    1st 6th commit

--- VCS Day2.js
+++ VCS Day2.js
@@ -1,7 +1,6 @@
 console.log("This is second branch");
 
-<<<<<<< HEAD
 console.log("This is master branch");
-=======
 console.log("This is merge test");
->>>>>>> feature/day-2
+
+console.log("This is master branch 123");
